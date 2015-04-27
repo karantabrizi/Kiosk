c776341027b6b2e4d14abb118cda30b3a02a79c5
KaranTabrizi <Karan.tabrizi@photoninfotech.net>
2015-04-27 17:52:12 -0500

    scisor and arrow. active and inactive

--- details/myCtrl.js
+++ details/myCtrl.js
@@ -2,11 +2,11 @@ app.controller("myCtrl", function($scope, $location) {
     $scope.mycid = $location.search().mycid;
     
     $scope.coupons = [
-        {"id":"1", "code":"ABCDEFGH", "discount":"20%", "title":"EXTRA 20% OFF WITH JCP CARD PURCHASE", "subtitle":"", "expiration":"Valid 4/27-5/17"},
-        {"id":"2", "code":"EFGHHIJK", "discount":"10%", "title":"$15 BONUSCASH", "subtitle":"WHEN YOU SPEND $50 OR MORE", "expiration":"Valid 4/27-5/17"},
+        {"id":"1", "code":"ABCDEFGH", "discount":"20%", "title":"EXTRA 20% OFF ", "subtitle":"WITH JCP CARD PURCHASE", "expiration":"Valid 4/27-5/17"},
+        {"id":"2", "code":"EFGHHIJK", "discount":"10%", "title":"$15 BONUS CASH", "subtitle":"WHEN YOU SPEND $50 OR MORE", "expiration":"Valid 4/27-5/17"},
         {"id":"3", "code":"HIJKLMNO", "discount":"15%", "title":"EXTRA 15% OFF", "subtitle":"WHEN YOU SPEND $40 OR MORE", "expiration":"Valid 4/27-5/17"},
-        {"id":"4", "code":"LMNOPQRW", "discount":"10%", "title":"$20 BONUSCASH", "subtitle":"WHEN YOU SPEND $60 OR MORE", "expiration":"Valid 4/27-5/17"},
-        {"id":"5", "code":"PQRSTUVW", "discount":"20%", "title":"EXTRA 20% OFF WITH JCP CARD PUCHASE", "subtitle":"", "expiration":"Valid 4/27-5/31"},
+        {"id":"4", "code":"LMNOPQRW", "discount":"10%", "title":"$20 BONUS CASH", "subtitle":"WHEN YOU SPEND $60 OR MORE", "expiration":"Valid 4/27-5/17"}, 
+        {"id":"5", "code":"PQRSTUVW", "discount":"20%", "title":"EXTRA 20% OFF ", "subtitle":"WITH JCP CARD PUCHASE", "expiration":"Valid 4/27-5/31"},
         {"id":"6", "code":"TUVWXYZA", "discount":"15%", "title":"EXTRA 15% OFF", "subtitle":"WHEN YOU SPEND $40 OR MORE", "expiration":"Valid 4/27-5/31"}
     ];
     
