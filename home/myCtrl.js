1998b8bdb1a5074ead028622928dad369926d557
Pooja Shyam <pooja.shyam@photoninfotech.net>
2015-04-30 12:04:19 -0500

    coupon changed

--- home/myCtrl.js
+++ home/myCtrl.js
@@ -4,7 +4,7 @@ app.controller("myCtrl", function($scope) {
     };
     
     $scope.coupons = [
-        {"id":"1", "code":"ABCDEFGH", "discount":"20%", "title":"EXTRA 20% OFF WITH JCP CARD PURCHASE", "subtitle":"", "expiration":"Valid 4/27-5/17", "image":"../images/coupon1.jpg"},
+        {"id":"1", "code":"ABCDEFGH", "discount":"20%", "title":"EXTRA 20% OFF WITH JCP CARD PURCHASE", "subtitle":"", "expiration":"Valid 4/27-5/17", "image":"../images/coupon5.jpg"},
         {"id":"2", "code":"EFGHHIJK", "discount":"10%", "title":"$15 BONUSCASH", "subtitle":"WHEN YOU SPEND $50 OR MORE", "expiration":"Valid 4/27-5/17", "image":"../images/coupon2.png"},
         {"id":"3", "code":"HIJKLMNO", "discount":"15%", "title":"EXTRA 15% OFF", "subtitle":"WHEN YOU SPEND $40 OR MORE", "expiration":"Valid 4/27-5/17", "image":"../images/coupon3.png"},
         {"id":"4", "code":"LMNOPQRW", "discount":"10%", "title":"$20 BONUSCASH", "subtitle":"WHEN YOU SPEND $60 OR MORE", "expiration":"Valid 4/27-5/17", "image":"../images/coupon4.png"},
